b3ffa26a49dfa6f8934326e45103e299607f5e79
ccdd <215136855@qq.com>
2024-12-06 22:01:42 +0800

    Add tool of convert torrent to magnet link

---
+++ Torrent2Magnet/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Security.Cryptography;
+
+var fileName = "";
+if (string.IsNullOrEmpty(fileName))
+{
+    fileName = args.First();
+}
+Console.WriteLine(ReadTorrent(fileName));
+
+
+static string ReadTorrent(string fileName)
+{
+    using (var stream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+    {
+        var torrent = new Honoo.Text.BEncode.TorrentAnalysis(stream, true);
+        Console.WriteLine("created by    :" + torrent.GetCreatedBy());
+        Console.WriteLine("creation date :" + torrent.GetCreationDate());
+        Console.WriteLine("announce      :" + torrent.GetAnnounce());
+        Console.WriteLine("comment       :" + torrent.GetComment());
+        Console.WriteLine("hash          :" + torrent.GetHash());
+        Console.WriteLine("nodes         :" + torrent.GetNodes());
+        Console.WriteLine("name          :" + torrent.GetName());
+        Console.WriteLine("piece length  :" + torrent.GetPieceLength());
+
+        var magnet = torrent.GetMagnet(true, true, true, false);
+        return magnet;
+    }
+}
